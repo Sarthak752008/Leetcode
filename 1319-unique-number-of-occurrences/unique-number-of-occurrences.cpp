c94d407ed6d5c06a61fb9e89315088c2cf31e858
Sarthak Sharma <sarthak752008@gmail.com>
2026-07-02 00:56:40 +0530

    Time: 3 ms (15.29%) | Memory: 12 MB (36.83%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> mp;
+
+        for (int x : arr)
+            mp[x]++;
+
+        vector<int> freq;
+        for (auto &it : mp)
+            freq.push_back(it.second);
+
+        sort(freq.begin(), freq.end());
+
+        for (int i = 1; i < freq.size(); i++) {
+            if (freq[i] == freq[i - 1])
+                return false;
+        }
+
+        return true;
+    }
+};
